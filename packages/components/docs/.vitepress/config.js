2728e9308b2120666d13c41461baa08b1f36c074
buzzlightyear <794758159@qq.com>
2021-09-14 22:07:50 +0800

    docs:nav & sidebar

--- packages/components/docs/.vitepress/config.js
+++ packages/components/docs/.vitepress/config.js
@@ -1,3 +1,14 @@
+const base = process.env.BASE || '/'
+const nav = require('./configs/nav')
+const sidebar = require('./configs/sidebar')
+
 module.exports = {
-    themeConfig: {},
+    title: 'Vue/Setup',
+    description: '组件库',
+    base: '/',
+    repo: 'vuejs/vitepress',
+    themeConfig: {
+        nav,
+        sidebar
+    }
 }
